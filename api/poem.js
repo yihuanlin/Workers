fbb0d71ba9eabd86836fa9f6fc9597d7b985617c
Yi, Huanlin <85040891+yihuanlin@users.noreply.github.com>
2025-01-01 16:35:21 +0000

    p

--- api/poem.js
+++ api/poem.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
                 }
             }
 
-            const newLength = Math.max(STARTKEY + putCount - 1);
+            const newLength = Math.max(STARTKEY + putCount + 1);
             await kv.set('length', newLength);
 
             return res.status(200).json({ success: true, totalLength: newLength, added: putCount });
