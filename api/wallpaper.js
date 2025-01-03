8343aba40ac53c3f1a076d987bc3e3b1f49c9528
Yi, Huanlin <85040891+yihuanlin@users.noreply.github.com>
2025-01-03 22:55:04 +0000

    Enhance wallpaper API to support mobile images and improve color extraction logic

--- api/wallpaper.js
+++ api/wallpaper.js
@@ -10,11 +10,7 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_REPO = process.env.GITHUB_REPO;
 const BING_API = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB';
 
-const EDGE_CONFIG_ID = () => {
-	if (!EDGE_CONFIG) return { id: null, token: null };
-	const idMatch = EDGE_CONFIG.match(/ecfg_[a-zA-Z0-9]+/);
-	return idMatch ? idMatch[0] : null;
-};
+const EDGE_CONFIG_ID = !EDGE_CONFIG ? null : EDGE_CONFIG.match(/ecfg_[a-zA-Z0-9]+/)?.[0] ?? null;
 
 const set = async (key, value) => {
 	const response = await fetch(`https://api.vercel.com/v1/edge-config/${EDGE_CONFIG_ID}/items`, {
@@ -166,42 +162,63 @@ const uploadToGithub = async (files, message) => {
 	return updates;
 }
 
+const getAvgColor = (stats) => '#' + [0, 1, 2]
+	.map(i => Math.min(255, Math.max(0, Math.round(stats.channels[i].mean)))
+		.toString(16).padStart(2, '0'))
+	.join('');
+
 const getBingWallpaper = async () => {
 	const response = await fetch(BING_API);
 	const data = await response.json();
 	const imageData = data.images[0];
 	const imageUrl = `https://www.bing.com${imageData.url.replace('1920x1080', 'UHD')}`;
-	const imageResponse = await fetch(imageUrl);
-	const arrayBuffer = await imageResponse.arrayBuffer();
+	const mobileImageUrl = `https://www.bing.com${imageData.url.replace('1920x1080', '1080x1920')}`;
 
-	const image = sharp(Buffer.from(arrayBuffer));
-	const { width, height } = await image.metadata();
-	const topRegionHeight = Math.floor(height * 0.05);
-	const topBuffer = await image
-		.resize({ width, height: topRegionHeight, position: 'top' })
-		.toBuffer();
-	const stats = await sharp(topBuffer).stats();
-	const avgColor =
-		'#' +
-		[
-			stats.channels[0].mean,
-			stats.channels[1].mean,
-			stats.channels[2].mean
-		]
-			.map(c => Math.min(255, Math.max(0, Math.round(c))).toString(16).padStart(2, '0'))
-			.join('');
+	const [desktopImage, mobileImage] = await Promise.all([
+		fetch(imageUrl)
+			.then(r => r.arrayBuffer())
+			.then(buffer => sharp(Buffer.from(buffer))),
+		fetch(mobileImageUrl)
+			.then(r => r.arrayBuffer())
+			.then(buffer => sharp(Buffer.from(buffer)))
+	]);
 
-	const webpBuffer = await sharp(Buffer.from(arrayBuffer))
-		.webp({ quality: 80 })
-		.toBuffer();
+	const [{ width: dWidth, height: dHeight }, { width: mWidth, height: mHeight }] = await Promise.all([
+		desktopImage.metadata(),
+		mobileImage.metadata()
+	]);
+
+	const [desktopData, mobileData] = await Promise.all([
+		desktopImage.toBuffer(),
+		mobileImage.toBuffer()
+	]);
+
+	const [desktopAvgColor, mobileAvgColor] = await Promise.all([
+		sharp(desktopData).resize({ width: dWidth, height: Math.floor(dHeight * 0.05), position: 'top' })
+			.stats()
+			.then(r => getAvgColor(r)),
+		sharp(mobileData).resize({ width: mWidth, height: Math.floor(mHeight * 0.05), position: 'top' })
+			.stats()
+			.then(r => getAvgColor(r))
+	]);
+
+	const [desktopWebpBuffer, mobileWebpBuffer] = await Promise.all([
+		await sharp(Buffer.from(desktopData))
+			.webp({ quality: 80 })
+			.toBuffer(),
+		await sharp(Buffer.from(mobileData))
+			.webp({ quality: 80 })
+			.toBuffer()
+	]);
 	const metadata = {
 		title: imageData.title,
 		link: imageData.copyrightlink,
 		description: imageData.copyright,
-		color: avgColor
+		color: desktopAvgColor,
+		mobileColor: mobileAvgColor,
 	};
 
-	return { image: webpBuffer, metadata };
+	return { image: desktopWebpBuffer, mobileImage: mobileWebpBuffer, metadata };
 };
 
 export default async (req, res) => {
@@ -211,6 +228,11 @@ export default async (req, res) => {
 		try {
 			if (req.query.type === 'image') {
 				const response = await fetch(`https://${BLOB_ID}.public.blob.vercel-storage.com/wallpaper.webp`);
+				const imageBuffer = await response.arrayBuffer();
+				res.setHeader('Content-Type', 'image/webp');
+				res.send(Buffer.from(imageBuffer));
+			} else if (req.query.type === 'mobile') {
+				const response = await fetch(`https://${BLOB_ID}.public.blob.vercel-storage.com/wallpaper-mobile.webp`);
 				const imageBuffer = await response.arrayBuffer();
 				res.setHeader('Content-Type', 'image/webp');
 				res.send(Buffer.from(imageBuffer));
@@ -224,10 +246,13 @@ export default async (req, res) => {
 			}
 		} catch {
 			try {
-				const { image, metadata } = await getBingWallpaper();
+				const { image, mobileImage, metadata } = await getBingWallpaper();
 				if (req.query.type === 'image') {
 					res.setHeader('Content-Type', 'image/webp');
 					res.send(image);
+				} if (req.query.type === 'mobile') {
+					res.setHeader('Content-Type', 'image/webp');
+					res.send(mobileImage);
 				} else {
 					res.status(200).json(metadata);
 				}
@@ -240,6 +265,10 @@ export default async (req, res) => {
 						access: 'public',
 						addRandomSuffix: false,
 						contentType: 'application/json'
+					}), put('wallpaper-mobile.webp', mobileImage, {
+						access: 'public',
+						addRandomSuffix: false,
+						contentType: 'image/webp'
 					}), set('wallpaper-metadata', metadata),
 					uploadToGithub([
 						{
@@ -249,6 +278,10 @@ export default async (req, res) => {
 						{
 							path: 'metadata.json',
 							content: Buffer.from(JSON.stringify(metadata)).toString('base64'),
+						},
+						{
+							path: 'wallpaper-mobile.webp',
+							content: mobileImage.toString('base64'),
 						}
 					], `Update wallpaper and description for ${date}`)
 				]));
@@ -259,7 +292,7 @@ export default async (req, res) => {
 	}
 	if (req.method === 'POST') {
 		try {
-			const { image, metadata } = await getBingWallpaper();
+			const { image, mobileImage, metadata } = await getBingWallpaper();
 			await Promise.all([
 				put('wallpaper.webp', image, {
 					access: 'public',
@@ -269,6 +302,10 @@ export default async (req, res) => {
 					access: 'public',
 					addRandomSuffix: false,
 					contentType: 'application/json'
+				}), put('wallpaper-mobile.webp', mobileImage, {
+					access: 'public',
+					addRandomSuffix: false,
+					contentType: 'image/webp'
 				}), set('wallpaper-metadata', metadata),
 				uploadToGithub([
 					{
@@ -278,6 +315,10 @@ export default async (req, res) => {
 					{
 						path: 'metadata.json',
 						content: Buffer.from(JSON.stringify(metadata)).toString('base64'),
+					},
+					{
+						path: 'wallpaper-mobile.webp',
+						content: mobileImage.toString('base64'),
 					}
 				], `Update wallpaper and description for ${date}`)
 			]);
